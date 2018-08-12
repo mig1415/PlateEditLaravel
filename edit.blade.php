5d7a22c18ad6c20604b48f4d6b36c2f37f836b35
mig1415 <41913862+mig1415@users.noreply.github.com>
2018-08-13 00:35:40 +0400

    Create edit.blade.php

---
+++ edit.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Редактор пластинок </div>
+                    <div class="panel-body">
+                        <form id="form">
+
+
+                            <?php
+                            echo 'вы выбрали альбом  ';
+                           echo '<font color=red>';
+                            print_r($tmpEditAlbum);
+                           echo '</font>';
+////////////////////////////////////////////////////////////////////////////////
+                           if ($tmpAlbumEdit or $tmpArtistEdit)  {
+                               echo '<script type="text/javascript">';
+                               echo 'window.location.href="/";';
+                               echo '</script>';
+
+                           }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                         ?>
+
+                            <br>
+                          Новое имя альбома: <input id="albumnew" name="albumnew"><br>
+                            <br>
+                                Новый исполнитель : <input id="artistnew" name="artistnew"> <br>
+
+                                <button>Сохранить</button>
+
+                        </form>
+                        <a href="/"><<<</a>
+
+
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
